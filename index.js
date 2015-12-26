02079c7322f41f5daab9abf6211a9a84d9c8e6a2
Helen Y <kenaileaf@gmail.com>
2015-12-26 15:36:15 -0800

    Added code.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var show = function(whichGame) {
-    $("\"#" + whichGame + "\"").show();
-}
+// var show = function(whichGame) {
+//     var id = "\"#" + whichGame + "\"";
+//     console.log(id);
+//     $(id).show();
+// }
 
 $(document).ready(function() {
     console.log("trying to hide");
     $("#wheelchair").hide();
     $("#vision").hide();
     $("#original").hide();
+});
+
+$("#wheelchairButton").click(function() {
+    $("#wheelchair").show();
+    
 });
