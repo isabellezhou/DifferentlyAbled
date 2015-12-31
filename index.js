b866453ec5b4b0988ead872e276ab5913c7d8f0c
Isabelle Zhou <isabelle.jq.zhou@gmail.com>
2015-12-31 12:39:07 -0800

    Sync fixes.

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 
 $(document).ready(function() {
-    // $("#begin").show();
-<<<<<<< Updated upstream
     $("#instructions").show();
     $("#wheelchair").hide();
-=======
     $("#wheelchair").show();
     $("#wheelchairCanvas").focus();
 	$("#wheelchairCanvas").scrollTop(0);
->>>>>>> Stashed changes
     $("#vision").hide();
     $("#original").hide();
     $("#memory").hide();
@@ -38,12 +34,9 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 	$("#memoryButton").click(function() {
-<<<<<<< Updated upstream
 		$("#instructions").hide();
 		$("#memory").show();
 	    $("#memoryCanvas").focus();
-=======
->>>>>>> Stashed changes
 	    $("#vision").hide();
 	    $("#wheelchair").hide();
         $("#original").hide();
@@ -53,12 +46,9 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $("#originalButton").click(function() {
-<<<<<<< Updated upstream
     	$("#instructions").hide();
     	$("#original").show();
         $("#originalCanvas").focus();
-=======
->>>>>>> Stashed changes
         $("#vision").hide();
         $("#wheelchair").hide();
         $("#memory").hide();
